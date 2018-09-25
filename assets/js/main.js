23b75d65bd6f7e3ca585cfc94501c31c4db3d5f3
Fedor Paretsky <me@f3d0r.com>
2018-09-25 16:23:18 -0700

    Correct behavior for form submission with only send

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ var messageBox = document.querySelector('.js-message');
 var btn = document.querySelector('.js-message-btn');
 var card = document.querySelector('.js-profile-card');
 var closeBtn = document.querySelectorAll('.js-message-close');
+var sendBtn = document.querySelectorAll('#send_button');
 
 btn.addEventListener('click', function (e) {
     e.preventDefault();
@@ -9,13 +10,12 @@ btn.addEventListener('click', function (e) {
 });
 
 closeBtn.forEach(function (element, index) {
-    console.log(element);
     element.addEventListener('click', function (e) {
         e.preventDefault();
-
-        var $form = $(".profile-card-form");
-        $.post($form.attr("action"), $form.serialize()).then(function () {});
-
+        if ($(element).get(0).id == "send_button") {
+            var $form = $(".profile-card-form");
+            $.post($form.attr("action"), $form.serialize()).then(function () {});
+        }
         card.classList.remove('active');
     });
 });
