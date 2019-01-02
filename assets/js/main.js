69cb339c6f21860f8e02f165408ce49fbe028cee
Fedor Paretsky <me@f3d0r.com>
2019-01-02 11:43:21 -0800

    Move form send into recaptcha callback

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,15 +13,14 @@ closeBtn.forEach(function (element, index) {
     element.addEventListener('click', function (e) {
         e.preventDefault();
         if ($(element).get(0).id == "send_button") {
-            var $form = $(".profile-card-form");
-            $.post($form.attr("action"), $form.serialize()).then(function () {});
-            $(".message_text").val("");
             grecaptcha.ready(function () {
                 grecaptcha.execute('6LcKUIYUAAAAAPQNdqTu8a51UzN3UDrvA9erXYnR', {
                         action: 'action_name'
                     })
                     .then(function (token) {
-                        console.log("TOKEN: " + token);
+                        var $form = $(".profile-card-form");
+                        $.post($form.attr("action"), $form.serialize()).then(function () {});
+                        $(".message_text").val("");
                     });
             });
         }
